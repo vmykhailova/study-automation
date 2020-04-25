16e9c439dfbc4a1671bd8f6328b61ad0075f0331
vmykhailova <fafra16042011@gmail.com>
2020-04-25 23:06:40 +0300

    Init Commit

---
+++ src/test/java/com/example/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void calculateTest () {
+        System.out.println("simple test");
+    }
+}
